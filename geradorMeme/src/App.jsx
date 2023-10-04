adc2dcdc7557463164de9eb77445c564a73d12fc
Alires Rosa <108585145+DomeniqueRosa@users.noreply.github.com>
2023-10-04 18:54:14 -0300

    first commit

---
+++ geradorMeme/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './style.css'
+
+function App() {
+
+  return (
+    <div>
+        <h1>Projeto 3 - Gerador meme.</h1>
+    </div>
+  )
+}
+
+export default App
