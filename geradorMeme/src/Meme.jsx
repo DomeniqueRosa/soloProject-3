2a2d0d03140ac46759fec284f59c81e225ee5d01
Alires Rosa <108585145+DomeniqueRosa@users.noreply.github.com>
2023-10-05 16:52:49 -0300

    adc evento

--- geradorMeme/src/Meme.jsx
+++ geradorMeme/src/Meme.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./memeData";
 
-export default function Meme(props){
-    const hand = function(){
-        console.log("MEme");
-    }
-    return(
-        <div className="form">
-            <form action="">
-                <div className="form--input">
-                    <input className="form--input-1"type="text" placeholder="Titulo"/>
-                    <input type="text" placeholder="Subtitilo" />
-                </div>
-                <input onClick={hand} className="btn" type="button" value="Gerar uma nova imagem de meme." />
-                <img  className="form--img"src="memeimg.png" alt="" />
-            </form>
-            
+export default function Meme() {
+  const [image, setImage] = useState("");
+
+  function getImage(e) {
+    e.preventDefault();
+    const arrayData = data.data.memes
+    const numeroRandom = Math.floor(Math.random() * arrayData.length)
+    const meme = arrayData[numeroRandom].url
+    setImage(meme)
+    console.log(setImage)
+    
+  }
+
+  return (
+    <div className="form">
+      <form action="">
+        <div className="form--input">
+          <input className="form--input-1" type="text" placeholder="Titulo" />
+          <input type="text" placeholder="Subtitilo" />
         </div>
-    )
+        <button onClick={getImage} className="btn">
+          Gerar uma nova imagem de meme.
+        </button>
+      </form>
+      <img src={image} alt="" className="form--img" />
+    </div>
+  );
 }
